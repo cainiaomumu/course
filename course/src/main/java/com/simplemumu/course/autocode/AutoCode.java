a3623dfcf1d482e4edf9f118a36ad05329756db6
cainiaomumu <15802141570@163.com>
2019-08-06 23:00:20 +0800

    代码生成

---
+++ course/src/main/java/com/simplemumu/course/autocode/AutoCode.java
@@ -0,0 +1,13 @@
+package com.simplemumu.course.autocode;
+/**
+ * -- 项目名称 : @course
+ * -- com.simplemumu.course.autocode
+ * -- 类的作用 : public class AutoCode{ }
+ * -- 创建时间 : 2019年8月6日
+ * -- 创建人 : linguanbin
+ * -- TODO
+ */
+public class AutoCode {
+
+}
+
